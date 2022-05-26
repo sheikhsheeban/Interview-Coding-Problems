484f389310b013af4070a71e145b17c07c5ca2f9
sheikhsheeban <67515460+sheikhsheeban@users.noreply.github.com>
2022-05-26 15:37:54 +0530

    Add files via upload

---
+++ 1st_and_last_occurence.py
@@ -0,0 +1,47 @@
+# Binary Search For Descending Order
+
+# Iterative Approach
+
+def binary_Search(arr,target):
+    start = 0
+    end = len(arr)-1
+    while start<=end:
+        mid=(start+end)//2
+        if target>arr[mid]:
+            start=mid+1
+        if target<arr[mid]:
+            end=mid-1
+        if target==arr[mid]:
+            result=mid
+            end=mid-1
+    return result
+
+
+arr=[ 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 ]
+target=8
+result = binary_Search(arr,target)
+if result!=None:
+    print("Element is present at index ",str(result))
+else:print("Element is not present in array")
+
+def binary_Search(arr,target):
+    start = 0
+    end = len(arr)-1
+    while start<=end:
+        mid=(start+end)//2
+        if target>arr[mid]:
+            start=mid+1
+        if target<arr[mid]:
+            end=mid-1
+        if target==arr[mid]:
+            result=mid
+            start=start+1
+    return result
+
+
+arr=[ 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 ]
+target=8
+result = binary_Search(arr,target)
+if result!=None:
+    print("Element is present at index ",str(result))
+else:print("Element is not present in array")
